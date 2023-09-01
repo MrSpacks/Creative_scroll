c1da66bcac638484fa769cb342137aeddbad494c
Mr. Spacks <94480894+MrSpacks@users.noreply.github.com>
2023-09-01 20:39:18 +0200

    Ready 100%

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-if (ScrollTrigger.isTouch !==1){
+if (ScrollTrigger.isTouch !==1) {
 
     ScrollSmoother.create({
         wrapper: '.wrapper',
@@ -10,11 +10,46 @@ if (ScrollTrigger.isTouch !==1){
     })
     gsap.fromTo('.hero_section', { opacity: 1 }, {
         opacity:0,
-        ScrollTrigger:{
-            trigger: 'hero_section',
+        scrollTrigger:{
+            trigger: '.hero_section',
             start: 'center',
-            end: 'bottom',
-            scrub: true
+            end: '900',
+            scrub: true,
         }
     })
+
+    let itemsLeft = gsap.utils.toArray('.gallery_left .gallery_item')
+    
+    itemsLeft.forEach(item =>{
+
+    gsap.fromTo(item, {x: -100, opacity: 0} ,{
+        opacity: 1, x: 0,
+        scrollTrigger:{
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true,
+        }
+    })
+
+   })
+   let itemsRight = gsap.utils.toArray('.gallery_right .gallery_item')
+    
+   itemsRight.forEach(item =>{
+
+   gsap.fromTo(item, {x: 100, opacity: 0} ,{
+       opacity: 1, x: 0,
+       scrollTrigger:{
+           trigger: item,
+           start: '-850',
+           end: '-100',
+           scrub: true,
+       }
+   })
+
+  })
 }
+
+
+
+
